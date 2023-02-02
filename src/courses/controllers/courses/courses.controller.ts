267dcc39812cb76da1efc39e94def48e3aa10f8e
Emil Baguioro I <emilbbaguioroi@gmail.com>
2023-02-02 16:55:02 +0800

    check if required fields are not empty

--- src/courses/controllers/courses/courses.controller.ts
+++ src/courses/controllers/courses/courses.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post, HttpException, HttpStatus } from '@nestjs/common';
+import { Body, Controller, Post, HttpException, HttpStatus, ValidationPipe } from '@nestjs/common';
+import { UsePipes } from '@nestjs/common/decorators/core/use-pipes.decorator';
 import { CreateCourseDto } from 'src/courses/dto/create-course.dto';
 import { CoursesService } from 'src/courses/services/courses/courses.service';
 
@@ -7,7 +8,9 @@ export class CoursesController {
 
     constructor(private readonly courseService: CoursesService) {}
 
+
     @Post('create')
+    @UsePipes(ValidationPipe)
     async createCourse(@Body() course: CreateCourseDto) {
         
         const createdCourse = await this.courseService.createCourse(course);
--- src/courses/dto/create-course.dto.ts
+++ src/courses/dto/create-course.dto.ts
@@ -1,8 +1,19 @@
+import { IsNotEmpty } from "class-validator";
+
 export class CreateCourseDto {
+
+    @IsNotEmpty()
     title: string;
+
+    @IsNotEmpty()
     desc: string;
+
     intro?: boolean;
+
     visibility?: boolean;
+
     videoUrl?: string;
+
+    @IsNotEmpty()
     category: string;
 }
