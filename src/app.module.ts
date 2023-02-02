05a4986d55ffe93b0cb520a38136e1e375678061
Emil Baguioro I <emilbbaguioroi@gmail.com>
2023-02-02 13:32:22 +0800

    make prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { CoursesModule } from './courses/courses.module';
 import { UsersService } from './users/services/users/users.service';
 import { UsersController } from './users/controllers/users/users.controller';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 
 @Module({
-  imports: [CoursesModule, AuthModule],
+  imports: [CoursesModule, AuthModule, PrismaModule],
   controllers: [UsersController],
   providers: [UsersService],
 })
